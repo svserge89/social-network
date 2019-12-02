e7b3de5b90c16148229ed52edb203a26a60e02e7
Serge Vorobyov <svserge89@gmail.com>
2019-12-02 22:01:25 +0300

    Use Redirect for Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import {compose} from 'redux';
+import {connect} from 'react-redux';
+import {Redirect} from 'react-router-dom';
 
-const Home = () => (<div>Home page</div>);
+const Home = ({userId}) => (
+  <Redirect to={userId ? "/profile" : "/login"}/>
+);
 
-export default Home;
+const mapStateToProps = ({auth: userId}) => ({userId});
+
+export default compose(connect(mapStateToProps))(Home);
