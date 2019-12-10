257205b6d9b4a07c3e797903a41f7d0234340998
Serge Vorobyov <svserge89@gmail.com>
2019-12-10 23:15:22 +0300

    Update realization for errorParser

--- src/utils/errorParser.js
+++ src/utils/errorParser.js
@@ -1,28 +1,21 @@
-const IN_BRACKETS = /\([^)]*\)/g;
-const BRACKETS = /[(,)]/g;
-const SPLIT = '->';
 const FIRST_LETTER = /^\w/;
+const TEMPLATE = /^(.+)\s+\((\w+)(->(\w+))?\)$/;
+const MESSAGE = 1;
+const FIELD = 2;
+const SUB_FIELD = 4;
 
 const toLowerCase = (word) => word.replace(FIRST_LETTER, ch => ch.toLowerCase());
 
-const getFieldNames = (message) => message.match(IN_BRACKETS);
-
-const getErrorString = (message, fieldName) => (
-  message.slice(0, message.length - fieldName.length - 1)
-);
-
-const getKeys = (fieldName) => fieldName.replace(BRACKETS, '').split(SPLIT).map(toLowerCase);
-
-const getPair = (prev, keys, message) => (
-  keys.length === 1
-    ? {...prev, [keys[0]]: message}
-    : {...prev, [keys[0]]: getPair(prev[keys[0]], keys.slice(1), message)}
-);
-
 export const parseMessages = (messages) => messages.reduce((prev, message) => {
-  const fieldNames = getFieldNames(message);
+  const match = TEMPLATE.exec(message);
 
-  if (!fieldNames) return {...prev, _error: message};
+  if (!match) return {...prev, _error: message};
 
-  return getPair(prev, getKeys(fieldNames[0]), getErrorString(message, fieldNames[0]));
+  const field = toLowerCase(match[FIELD]);
+
+  if (!match[SUB_FIELD]) return {...prev, [field]: match[MESSAGE]};
+
+  const subField = toLowerCase(match[SUB_FIELD]);
+
+  return {...prev, [field]: {...prev[field], [subField]: match[MESSAGE]}};
 }, {});
