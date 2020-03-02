8d14a64bc3f1c359be813166fd1a96cbadf8eb6c
Serge Vorobyov <svserge89@gmail.com>
2020-03-02 11:43:47 +0300

    Update page footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,9 @@ import Layout from '../Layout/Layout';
 
 const Footer = () => (
   <footer className="py-2 bg-light">
-    <Layout className="px-2">
+    <Layout className="px-2 text-right">
       <span className="text-muted">
-        Copyright © 2020 <a href="https://github.com/svserge89">svserge89</a>.
+        Copyright © 2020 <a href="https://github.com/svserge89">Serge Vorobyov</a>.
       </span>
     </Layout>
   </footer>
