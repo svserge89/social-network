7347a3ed0a2d764e483fa6377d2cfd4b8a3995dd
Serge Vorobyov <svserge89@gmail.com>
2020-03-04 18:01:11 +0300

    Refactoring Layout component for TypeScript

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import {Container} from 'react-bootstrap';
 import cn from 'classnames';
 
+import {LayoutProps} from './types';
+
 import style from './Layout.module.css';
 
-const Layout = ({children, className}) => (
+const Layout: React.FC<LayoutProps> = ({children, className}) => (
   <Container className={cn(style.container, className)}>{children}</Container>
 );
 
