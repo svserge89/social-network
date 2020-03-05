b516f122830ef3677c702b80a9cbb02cf78cf0d0
Serge Vorobyov <svserge89@gmail.com>
2020-03-05 11:58:39 +0300

    Refactoring NotMatch component for TypeScript

--- src/components/NotMatch/NotMatch.tsx
+++ src/components/NotMatch/NotMatch.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Error from '../Error/Error';
 
-const NotMatch = () => (
+const NotMatch: React.FC = () => (
   <Error code={404} description={`Bad URL path: "${window.location.href}"`}/>
 );
 
