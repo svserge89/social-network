cf75d1f3fd8116ad2ec3a577ec3dabef21669937
Serge Vorobyov <svserge89@gmail.com>
2020-07-21 20:35:06 +0300

    Refactoring tests for init thunk

--- src/reducers/init/__tests__/thunks.ts
+++ src/reducers/init/__tests__/thunks.ts
@@ -4,15 +4,9 @@ import thunk from 'redux-thunk';
 import authAPI from '../../../api/auth';
 import {RootState} from '../../../store/types';
 import {initialization} from '../thunks';
-import {InitAction} from '../types';
 import {AuthAction} from '../../auth/types';
 import {ErrorAction} from '../../error/types';
-import {
-  SET_CURRENT_USER_ACTION,
-  SET_FETCHING_FALSE_ACTION,
-  SET_FETCHING_TRUE_ACTION,
-  SUCCESS_GET_CURRENT_USER_RESPONSE
-} from '../../auth/__fixtures__/data';
+import {SET_CURRENT_USER_ACTION, SUCCESS_GET_CURRENT_USER_RESPONSE} from '../../auth/__fixtures__/data';
 import {ROOT_STATE, SET_INITIALIZED_TRUE_ACTION} from '../__fixtures__/data';
 import {ERROR_RESPONSE, SET_ERROR_ACTION} from '../../error/__fixtures__/data';
 import {DispatchExts} from '../__fixtures__/types';
@@ -32,28 +26,22 @@ describe('init thunk actions', () => {
 
     it('should dispatch actions when success response', async () => {
       spyGetCurrentUser.mockResolvedValue(SUCCESS_GET_CURRENT_USER_RESPONSE);
-      await store.dispatch(initialization());
-
-      expect(spyGetCurrentUser).toBeCalledTimes(1);
-      expect(store.getActions()).toEqual<(InitAction | AuthAction)[]>([
-        SET_FETCHING_TRUE_ACTION,
-        SET_CURRENT_USER_ACTION,
-        SET_FETCHING_FALSE_ACTION,
-        SET_INITIALIZED_TRUE_ACTION
-      ]);
+      await testInitialization(SET_CURRENT_USER_ACTION);
     });
 
     it('should dispatch actions when error response', async () => {
       spyGetCurrentUser.mockRejectedValue(ERROR_RESPONSE);
+      await testInitialization(SET_ERROR_ACTION);
+    });
+
+    async function testInitialization(expectedAction: AuthAction | ErrorAction): Promise<void> {
       await store.dispatch(initialization());
 
+      const actions = store.getActions();
+
       expect(spyGetCurrentUser).toBeCalledTimes(1);
-      expect(store.getActions()).toEqual<(InitAction | AuthAction | ErrorAction)[]>([
-        SET_FETCHING_TRUE_ACTION,
-        SET_ERROR_ACTION,
-        SET_FETCHING_FALSE_ACTION,
-        SET_INITIALIZED_TRUE_ACTION
-      ]);
-    });
+      expect(actions).toContainEqual(expectedAction);
+      expect(actions[actions.length - 1]).toEqual(SET_INITIALIZED_TRUE_ACTION);
+    }
   });
 });
