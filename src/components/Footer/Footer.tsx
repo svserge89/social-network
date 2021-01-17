9f8fcab2b64c64db660023ff8fc2c0cd88fe095d
Serge Vorobyov <svserge89@gmail.com>
2021-01-17 20:13:54 +0300

    Update Footer for current year

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,8 +6,11 @@ const Footer: React.FC = () => (
   <footer className="py-2 bg-light">
     <Layout className="px-2 text-right">
       <span className="text-muted">
-        Copyright © 2020{' '}
-        <a href="https://github.com/svserge89">Serge Vorobyov</a>.
+        Copyright © {new Date().getFullYear()}
+        <a className="pl-2" href="https://github.com/svserge89">
+          Serge Vorobyov
+        </a>
+        .
       </span>
     </Layout>
   </footer>
